14ce6069a673bc2991f00764d95f794319f86264
coderamrin <coderamrin@gmail.com>
2021-12-14 12:28:40 +0600

    finished the hero section of easy-bank project

--- easy-bank-landing-page/script.js
+++ easy-bank-landing-page/script.js
@@ -1,1 +1,8 @@
-console.log('hello');
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('open');
+
+  navLinks.classList.toggle('show'); 
+});
